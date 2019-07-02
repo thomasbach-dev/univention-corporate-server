4820abc583d9915ab48a8d000f87bba4066428d9
Philipp Hahn <hahn@univention.de>
2019-07-02 10:24:59 +0200

    Bug #49704 ucslint: Add JUnit XML support

--- packaging/ucslint/univention/ucslint/base.py
+++ packaging/ucslint/univention/ucslint/base.py
@@ -31,6 +31,22 @@
 import os
 from codecs import open
 try:
+	from junit_xml import TestCase  # type: ignore
+	JUNIT = True
+except ImportError:
+	JUNIT = False
+
+	class TestCase(object):  # type: ignore
+		def __init__(self, name, stdout=None, file=None, line=None):
+			pass
+
+		def add_error_info(self, message=None, output=None, error_type=None):
+			pass
+
+		def add_skipped_info(self, message=None, output=None):
+			pass
+
+try:
 	from typing import Any, Dict, Iterable, Iterator, List, Pattern, Optional, Tuple  # noqa F401
 except ImportError:
 	pass
@@ -91,6 +107,16 @@ class UPCMessage(object):
 		Return unique message identifier.
 		"""
 		return self.id
+
+	def junit(self):
+		# type: () -> TestCase
+		"""
+		Return JUnit XML test case.
+
+		:returns: test case.
+		"""
+		tc = TestCase(self.id, stdout=self.msg, file=self.filename, line=self.line)
+		return tc
 
 
 class UniventionPackageCheckBase(object):
