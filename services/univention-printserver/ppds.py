thomasbach-dev/univention-corporate-server

commit d42af4bfa479e347bad859263a1e0ce9c6a3e0f9

Bug #49174: futurize univention-printserver

services/univention-printserver/ppds.py
@@ -32,6 +32,8 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
+from __future__ import print_function
+
 import os
 import gzip
 import univention.uldap
@@ -77,7 +79,7 @@ def check_obsolete():
 	# check old models
 	lo = univention.uldap.getMachineConnection()
 	res = lo.search(filter='(objectClass=univentionPrinterModels)', attr=['printerModel', 'cn'])
-	print '# mark old ppd\'s as obsolete\n'
+	print('# mark old ppd\'s as obsolete\n')
 	for dn, attr in res:
 		cn = attr.get('cn')[0]
 		obsolete = dict()
@@ -91,15 +93,15 @@ def check_obsolete():
 					obsolete[cn] = list()
 				obsolete[cn].append(i)
 		for cn in obsolete:
-			print '/usr/lib/univention-printserver/univention-ppds/mark_models_as_deprecated.py "$@" --name "%s" \\' % cn
-			print '\t\'' + '\' \\\n\t\''.join(obsolete[cn]) + '\''
+			print('/usr/lib/univention-printserver/univention-ppds/mark_models_as_deprecated.py "$@" --name "%s" \\' % cn)
+			print('\t\'' + '\' \\\n\t\''.join(obsolete[cn]) + '\'')
 
 
 if __name__ == '__main__':
 	printers = {}
 	cmds = []
 	os.path.walk('/usr/share/ppd/', __check_dir, printers)
-	for manu, models in printers.items():
+	for manu, models in list(printers.items()):
 		cmds.append(get_udm_command(manu, models))
-	print '\n\n'.join(cmds)
+	print('\n\n'.join(cmds))
 	check_obsolete()

services/univention-printserver/mark_models_as_deprecated.py
@@ -28,6 +28,8 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
+from __future__ import print_function
+
 import os
 import gzip
 import optparse
@@ -104,8 +106,8 @@ class UpdatePrinterModels(object):
 			model_diff = set(ldap_models).difference(new_ldap_models)
 			if model_diff:
 				if self.options.verbose:
-					print 'removing duplicate models for %s:' % dn
-					print '\t' + '\n\t'.join(model_diff)
+					print('removing duplicate models for %s:' % dn)
+					print('\t' + '\n\t'.join(model_diff))
 				if not options.dry_run:
 					changes = [('printerModel', ldap_models, new_ldap_models)]
 					self.lo.modify(dn, changes)
@@ -124,16 +126,16 @@ class UpdatePrinterModels(object):
 					changed = True
 					obj['printmodel'].remove(model_item)
 					if options.verbose:
-						print 'info: %s model "%s" removed' % (obj.dn, model_item)
+						print('info: %s model "%s" removed' % (obj.dn, model_item))
 					model_item[1] = 'deprecated (only available in %s and older) - ' % self.options.version + model_item[1]
 					obj['printmodel'].append(model_item)
 					if options.verbose:
-						print 'info: %s model "%s" added' % (obj.dn, model_item)
+						print('info: %s model "%s" added' % (obj.dn, model_item))
 			if changed:
 				if not options.dry_run:
 					obj.modify()
 				if options.verbose:
-					print 'info: %s modified' % obj.dn
+					print('info: %s modified' % obj.dn)
 
 
 if __name__ == '__main__':
@@ -156,4 +158,4 @@ if __name__ == '__main__':
 		upm.check_duplicates()
 	else:
 		if options.verbose:
-			print 'info: do nothing, no model and/or name given'
+			print('info: do nothing, no model and/or name given')

services/univention-printserver/univention-cupsadmin
@@ -32,6 +32,7 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
+from __future__ import print_function
 import sys
 import string
 import re
@@ -88,4 +89,4 @@ while not i == 2:
 		child.sendline(machine_password)
 	elif i == 2:
 		if child.before:
-			print child.before
+			print(child.before)

services/univention-printserver/univention-lpadmin
@@ -31,6 +31,7 @@
 # /usr/share/common-licenses/AGPL-3; if not, see
 # <https://www.gnu.org/licenses/>.
 
+from __future__ import print_function
 import sys
 import re
 import pwd
@@ -75,7 +76,7 @@ def show_child_before_message(child):
 	""" Method shows only lpadmin error/info message that comes only with "lpadmin: """
 	for stdout_line in child.before.splitlines():
 		if "lpadmin: " in stdout_line:
-			print stdout_line.strip()
+			print(stdout_line.strip())
 
 
 def enableaccept(command, groupname, timeout, machine_password):
@@ -93,7 +94,7 @@ def enableaccept(command, groupname, timeout, machine_password):
 		child.close()
 
 	if child.exitstatus and child.exitstatus != 0:
-		print 'The command "%s %s" returned %d' % (command, groupname, child.exitstatus)
+		print('The command "%s %s" returned %d' % (command, groupname, child.exitstatus))
 		sys.exit(child.exitstatus)
 
 
@@ -105,9 +106,9 @@ while not i == 3:
 		attempt += 1
 
 	if attempt > 3:
-		print 'Failed to create the printer with the following command: '
-		print '  %s %s' % (lpadminexec, ' '.join(args))
-		print 'Please check the machine account or the PAM configuration.'
+		print('Failed to create the printer with the following command: ')
+		print('  %s %s' % (lpadminexec, ' '.join(args)))
+		print('Please check the machine account or the PAM configuration.')
 		sys.exit(1)
 
 
@@ -117,7 +118,7 @@ if child.isalive():
 	child.close()
 
 if child.exitstatus and child.exitstatus != 0:
-	print 'The command "%s %s" returned %d' % (lpadminexec, ' '.join(args), child.exitstatus)
+	print('The command "%s %s" returned %d' % (lpadminexec, ' '.join(args), child.exitstatus))
 	sys.exit(child.exitstatus)
 
 # check for several classes

commit 5bb7e0ec91e6330ca73adb36745f5f7fc6b6a0c8

Bug #49174 print: Undo futurize
Fixes: d42af4bfa479e347bad859263a1e0ce9c6a3e0f9

services/univention-printserver/ppds.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
 	printers = {}
 	cmds = []
 	os.path.walk('/usr/share/ppd/', __check_dir, printers)
-	for manu, models in list(printers.items()):
+	for manu, models in printers.items():
 		cmds.append(get_udm_command(manu, models))
 	print('\n\n'.join(cmds))
 	check_obsolete()