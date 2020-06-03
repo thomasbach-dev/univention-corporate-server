0acc67a6d6e702b33670b2707a95cae3a6a55543
Max Christian Pohle <pohle@univention.de>
2020-06-03 16:52:46 +0200

    Fixed several mistakes

--- test/ucs-test/tests/86_selenium/11_umc_pam-login_unclosed_fh_test.py
+++ test/ucs-test/tests/86_selenium/11_umc_pam-login_unclosed_fh_test.py
@@ -6,39 +6,41 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from xvfbwrapper import Xvfb
 
-import sys
 import time
 import subprocess
 
 import univention.testing.udm as udm_test
+import univention.testing.ucr as ucr_test
+
 
 class OpenFdTests(unittest.TestCase):
+    """
+    This test checks problems caused by open file descriptors.
+    """
 
     def setUp(self):
         self.xvfb = Xvfb(width=1280, height=720)
         self.addCleanup(self.xvfb.stop)
         self.xvfb.start()
+
         self.browser = webdriver.Firefox()
         self.addCleanup(self.browser.quit)
 
         with ucr_test.UCSTestConfigRegistry() as ucr:
             self.domainname = ucr.get('domainname')
 
-    def count_fhs():
+    def count_fhs(self):
         umc_pid = int(subprocess.check_output('pidof -x univention-management-console-server'.split(' ')))
         return int(subprocess.check_output(['bash', '-c', 'lsof -p ' + umc_pid + ' | grep 7389 | wc -l']))
 
-    def restart_umc():
+    def restart_umc(self):
         subprocess.check_call(['systemctl', 'restart', 'univention-management-console-server'])
         """ HINT: check_call: Run command with arguments. Wait for command to complete.  """
 
-    def test_self_service_change_password():
+    def test_self_service_change_password(self):
         """
         function to visit the self-service page 'passwordchange' and set a new
         password. The test fails, if the number of open file descriptors has
@@ -53,48 +55,49 @@ class OpenFdTests(unittest.TestCase):
         with udm_test.UCSTestUDM() as udm:
             userdn, username = udm.create_user(set={'password': password_before})
 
-
-        before_fhs = count_fhs()
+        before_fhs = self.count_fhs()
         try:
-            self.browser.get("http://localhost/univention/self-service/#page=passwordchange")
+            self.browser.get("http://" + self.domainname + "/univention/self-service/#page=passwordchange")
+            self.browser.save_screenshot('/tmp/self_service.png')
             self.browser.find_element(By.ID, "selfservice_password_TextBox_5").send_keys(username)
-            self.browser.find_element(By.ID, "selfservice_password_TextBox_6").send_keys(old_pass)
-            self.browser.find_element(By.ID, "selfservice_password_PasswordBox_2").send_keys(new_pass)
-            self.browser.find_element(By.ID, "selfservice_password_TextBox_7").send_keys(new_pass + Keys.RETURN)
+            self.browser.find_element(By.ID, "selfservice_password_TextBox_6").send_keys(password_before)
+            self.browser.find_element(By.ID, "selfservice_password_PasswordBox_2").send_keys(password_after)
+            self.browser.find_element(By.ID, "selfservice_password_TextBox_7").send_keys(password_after + Keys.RETURN)
             WebDriverWait(self.browser, 10).until(
                     expected_conditions.presence_of_element_located((By.ID, "umc_widgets_ConfirmDialog_0")))
+            self.browser.save_screenshot('/tmp/change_password.png')
         finally:
             self.browser.find_element(By.ID, "umc_widgets_Button_3").send_keys(Keys.RETURN) # ok button click
-        after_fhs = count_fhs()
+        after_fhs = self.count_fhs()
 
         self.assertEqual(before_fhs, after_fhs)
 
-
-    def umc_logon(username, pw):
+    def umc_logon(self, username, pw):
         """
         method to log into the ucs portal with a given username and password
         """
 
         try:
-                self.browser.get('https://ucs-9720/univention/portal/')
+            self.browser.get('https://' + self.domainname + '/univention/portal/')
 
-                Webself.browserWait(self.browser, 20).until(
-                        expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="umcLoginButton_label"]'))).click()
-                Webself.browserWait(self.browser, 20).until(
-                        expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="umcLoginUsername"]'))).send_keys(username)
-                Webself.browserWait(self.browser, 20).until(
-                        expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="umcLoginPassword"]'))).send_keys(pw)
+            self.browser.Wait(self.browser, 20).until(
+                 expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="umcLoginButton_label"]'))).click()
+            self.browser.Wait(self.browser, 20).until(
+                 expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="umcLoginUsername"]'))).send_keys(username)
+            self.browser.Wait(self.browser, 20).until(
+                    expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="umcLoginPassword"]'))).send_keys(pw)
 
-                elem = self.browser.find_elements_by_id('umcLoginSubmit')[0]
-                elem.click()
+            elem = self.browser.find_elements_by_id('umcLoginSubmit')[0]
+            elem.click()
 
-                Webself.browserWait(self.browser, 20).until(
-                        expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="umcLoginButton_label"]'))).click()
+            self.browser.Wait(self.browser, 20).until(
+                    expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="umcLoginButton_label"]'))).click()
+
+            self.browser.save_screenshot('/tmp/umc_logon.png')
         finally:
-                print('UMC Logon with {} done'.format(username))
+            print('UMC Logon with {} done'.format(username))
 
-
-    def test_umc_logon():
+    def test_umc_logon(self):
         """
         count the number of open file handles in the CLOSE_WAIT state after
         several logins
@@ -108,12 +111,12 @@ class OpenFdTests(unittest.TestCase):
             userdn, username = udm.create_user(set={'password': password})
 
         for i in range(0, 4):
-            umc_logon(username, password)
+            self.umc_logon(username, password)
 
         time.sleep(60)
         subprocess.check_call(['service', 'slapd', 'restart'])
 
-        umc_logon(username, password)
+        self.umc_logon(username, password)
 
         umc_pid = subprocess.check_output(['pidof', '-x', 'univention-management-console-server']).strip()
         umc_lsof = subprocess.check_output(['lsof', '-p', umc_pid])
@@ -123,9 +126,8 @@ class OpenFdTests(unittest.TestCase):
             if 'CLOSE_WAIT' in line:
                 close_wait += 1
 
-        assertEqual(close_wait, 0)
+        self.assertEqual(close_wait, 0)
 
 
 if __name__ == '__main__':
     unittest.main()
-
